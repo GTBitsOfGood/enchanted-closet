34ab8e01c268c044abfac8ed4a7aaee68954c725
Joe Torraca <joe@joetorraca.com>
2017-10-10 22:05:15 -0400

    Include db file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const db = require('./backend/model/db');
 const api = require('./backend/routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
