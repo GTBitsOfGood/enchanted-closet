91988784eb990b069bf594538522fac1d5047f56
Joe Torraca <joe@joetorraca.com>
2017-10-24 15:09:17 -0400

    Click "login" redirects to login page

--- frontend/containers/Navigation.js
+++ frontend/containers/Navigation.js
@@ -1,46 +1,61 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Menu, Dropdown } from 'semantic-ui-react'
 
-const Navigation = ({performLogout, loggedIn}) => {
-    const activeColor = 'violet'
-    return (
-    	<Menu color='grey' inverted stackable size='huge'>
-            <Menu.Item header color={activeColor}>Enchanted Closet</Menu.Item>
-            {loggedIn &&
-            <div>
+import { withRouter } from 'react-router-dom';
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.navigate = this.navigate.bind(this);
+    }
+
+    navigate(route) {
+        this.props.history.push(route);
+    }
+
+    render() {
+        const { applicationName, loggedIn, performLogout } = this.props;
+        const activeColor = 'violet'
+        return (
+        	<Menu color='grey' inverted stackable size='large'>
+                <Menu.Item header color={activeColor}>{applicationName}</Menu.Item>
+                {loggedIn &&
                 <Menu.Menu position='right'>
                     <Dropdown item text='Admin'>
                         <Dropdown.Menu>
-                            <Dropdown.Item>Dashboard</Dropdown.Item>
+                            <Dropdown.Item onClick={() => this.navigate('/admin/dashboard')}>Dashboard</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </Menu.Menu>
+                }
+                {loggedIn &&
                 <Menu.Item color={activeColor} onClick={performLogout}>Log out</Menu.Item>
-            </div>
-            }
-            {!loggedIn &&
-            <Menu.Item position='right' active color={activeColor} onClick={performLogout}>Log In</Menu.Item>
-            }
-	    </Menu>
-    );
+                }
+                {!loggedIn &&
+                <Menu.Item position='right' active color={activeColor} onClick={() => {this.navigate('/login')}}>Log In</Menu.Item>
+                }
+    	    </Menu>
+        );
+    };
 };
 
 const mapStateToProps = (state) => {
     return {
-        loggedIn: false
+        applicationName: state.applicationName,
+        loggedIn: true
     };
 };
 
-const mapDispatchToProps = (dispatch ) => ({
+const mapDispatchToProps = (dispatch) => ({
     performLogout() {
         console.log('Will logout');
         return {};
     }
 });
 
-export default connect(
+export default withRouter(connect(
     mapStateToProps,
     mapDispatchToProps
-)(Navigation);
+)(Navigation));
