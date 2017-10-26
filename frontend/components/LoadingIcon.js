35035c2fc9f9e67f975e5cc162c70e0ca128dca9
Joe Torraca <joe@joetorraca.com>
2017-10-26 13:18:36 -0400

    Create LoadingIcon wrapper

---
+++ frontend/components/LoadingIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Loader } from 'semantic-ui-react';
+
+const LoadingIcon = ( props ) => {
+	const { text, active } = props;
+    return (
+        <Loader {...props} inline='centered'>{text || 'Loading'}</Loader>
+    );
+};
+
+export default LoadingIcon;
