e1bd073a246e331420f3d82408dbc98e7081a149
Joe Torraca <joe@joetorraca.com>
2017-10-29 16:44:16 -0400

    Add link to event creation page

--- frontend/components/PageTitle.js
+++ frontend/components/PageTitle.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { Container, Card } from 'semantic-ui-react';
+import { Container, Card, Grid } from 'semantic-ui-react';
 
-const PageTitle = ( props ) => {
-	const { title } = props;
-	return (
-		<Container>
-			<Card fluid>
-				<Card.Content><h2>{title}</h2></Card.Content>
-			</Card>
-		</Container>
-	);
-};
+import { withRouter } from 'react-router-dom';
 
-export default PageTitle;
+class PageTitle extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+    render() {
+    	const { title, link, linkTitle } = this.props;
+    	return (
+    		<Container>
+    			<Card fluid>
+    				<Card.Content>
+    					<Grid>
+    						<Grid.Row columns={link && linkTitle ? 2 : 1}>
+    							<Grid.Column>
+    								<h2>{title}</h2>
+    							</Grid.Column>
+    							{link && linkTitle &&
+    							<Grid.Column textAlign="right">
+	    							<a href={`#/${link}`}>{linkTitle}</a>
+    							</Grid.Column>
+	    						}
+    						</Grid.Row>
+    					</Grid>
+    				</Card.Content>
+    			</Card>
+    		</Container>
+    	);
+    }
+}
+
+export default withRouter(PageTitle);
