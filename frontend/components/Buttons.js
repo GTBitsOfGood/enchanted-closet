82a9f84f214cfecb097e664a4bf5495d40d25615
Joe Torraca <joe@joetorraca.com>
2017-11-18 23:21:03 -0500

    Ensure routes are absolute

--- frontend/components/Buttons.js
+++ frontend/components/Buttons.js
@@ -4,7 +4,7 @@ import { Button, Icon } from 'semantic-ui-react';
 
 const editButton = ({history, route, id}) => {
     return (
-        <Button secondary animated="vertical" onClick={() => history.push(`${route}/${id}`)}>
+        <Button secondary animated="vertical" onClick={() => history.push(`/${route}/${id}`)}>
             <Button.Content visible>Edit</Button.Content>
             <Button.Content hidden>
               <Icon name='pencil' />
