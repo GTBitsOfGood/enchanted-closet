0292bb759e9e192add8588e1d17ddc692828586e
Joe Torraca <joe@joetorraca.com>
2017-11-19 00:52:21 -0500

    Tabs > spaces

--- frontend/containers/EventsDetail.js
+++ frontend/containers/EventsDetail.js
@@ -14,94 +14,94 @@ import {uniqueId} from 'lodash';
 import { withRouter } from 'react-router-dom';
 
 class EventsDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.handleRefreshClick = this.handleRefreshClick.bind(this)
-	const { match } = this.props;
-	const eventId = match.params.id;
-	this.state = {
-	    detail : '',
-	    eventId
+	constructor(props) {
+		super(props);
+		this.handleRefreshClick = this.handleRefreshClick.bind(this)
+		const { match } = this.props;
+		const eventId = match.params.id;
+		this.state = {
+			detail : '',
+			eventId
+		}
 	}
-    }
 
-    componentDidMount() {
-        const { dispatch, events, location } = this.props;
-	dispatch(fetchEventsIfNeeded());	
-        const detail = events.filter(event => event._id === this.state.eventId);
-	if (detail.length === 0) { //in case local store is old
-	    dispatch(fetchEvents());
-	} else {
-	    this.setState( {detail: detail[0]} );
+	componentDidMount() {
+		const { dispatch, events, location } = this.props;
+		dispatch(fetchEventsIfNeeded());	
+		const detail = events.filter(event => event._id === this.state.eventId);
+		if (detail.length === 0) { //in case local store is old
+			dispatch(fetchEvents());
+		} else {
+			this.setState( {detail: detail[0]} );
+		}
 	}
-    }
 
-    componentWillReceiveProps(nextProps) {
-	const { events } = nextProps;
-	const eventId = this.state.eventId;
-	//process it again
-	const detail = events.filter((event) => {
-	    return event._id === eventId})
-	this.setState({detail: detail.length === 0 ? '' : detail[0]})
+	componentWillReceiveProps(nextProps) {
+		const { events } = nextProps;
+		const eventId = this.state.eventId;
+		//process it again
+		const detail = events.filter((event) => {
+			return event._id === eventId})
+		this.setState({detail: detail.length === 0 ? '' : detail[0]})
 
-    }
-    
-    handleRefreshClick(e) {
-        e.preventDefault()
+	}
 
-        const { dispatch } = this.props;
-        dispatch(invalidateEvents())
-        dispatch(fetchEventsIfNeeded());
-    }
+	handleRefreshClick(e) {
+		e.preventDefault()
 
-    render() {
-        const { events, isFetchingEvents, lastUpdatedEvents, location } = this.props;
-	const detail = this.state.detail;
-        return (
-            <Container>
-                <h1>Showing event : {this.state.eventId}</h1>
-                {lastUpdatedEvents &&
-                 <span>Last updated at {new Date(lastUpdatedEvents).toLocaleTimeString()}.{' '}</span>
-                }
-                {!isFetchingEvents &&
-                 <a href="#" onClick={this.handleRefreshClick}>
-                     Refresh
-                 </a>}
-                <Dimmer active={isFetchingEvents}>
-                    <Loader>Loading</Loader>
-                </Dimmer>
-                { detail != '' &&
-                  Event(detail)
-                }
-                { !isFetchingEvents && detail === '' && 
-                  (
-		      <ErrorComponent redir='#/events/' redirMsg='Return to all events' errMsg='404 - Event not Found'/>
-		  )
-                }
-            </Container>
-        );
-    }
+		const { dispatch } = this.props;
+		dispatch(invalidateEvents())
+		dispatch(fetchEventsIfNeeded());
+	}
+
+	render() {
+		const { events, isFetchingEvents, lastUpdatedEvents, location } = this.props;
+		const detail = this.state.detail;
+		return (
+			<Container>
+			<h1>Showing event : {this.state.eventId}</h1>
+			{lastUpdatedEvents &&
+				<span>Last updated at {new Date(lastUpdatedEvents).toLocaleTimeString()}.{' '}</span>
+			}
+			{!isFetchingEvents &&
+				<a href="#" onClick={this.handleRefreshClick}>
+				Refresh
+				</a>}
+				<Dimmer active={isFetchingEvents}>
+				<Loader>Loading</Loader>
+				</Dimmer>
+				{ detail != '' &&
+				Event(detail)
+			}
+			{ !isFetchingEvents && detail === '' && 
+			(
+				<ErrorComponent redir='#/events/' redirMsg='Return to all events' errMsg='404 - Event not Found'/>
+				)
+		}
+		</Container>
+		);
+	}
 }
 
 EventsDetail.propTypes = {
-  events: PropTypes.array.isRequired,
-  isFetchingEvents: PropTypes.bool.isRequired,
-  lastUpdatedEvents: PropTypes.number,
-  dispatch: PropTypes.func.isRequired
+	events: PropTypes.array.isRequired,
+	isFetchingEvents: PropTypes.bool.isRequired,
+	lastUpdatedEvents: PropTypes.number,
+	dispatch: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state) {
-    const {
-        isFetchingEvents,
-        lastUpdatedEvents,
-        events
-    } = state;
+	const {
+		isFetchingEvents,
+		lastUpdatedEvents,
+		events
+	} = state;
 
-    return {
-        events,
-        isFetchingEvents,
-        lastUpdatedEvents
-    }
+	return {
+		events,
+		isFetchingEvents,
+		lastUpdatedEvents
+	}
 }
 
 export default withRouter(connect(mapStateToProps)(EventsDetail))
