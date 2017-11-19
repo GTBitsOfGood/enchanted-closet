fa202ea3544d682f7b3826c7d37dbe4271b5fc80
Sam <sam.scheele@outlook.com>
2017-11-19 12:54:44 -0500

    about to switch branches, hold this for me

--- backend/routes.js
+++ backend/routes.js
@@ -5,10 +5,13 @@ const router = express.Router();
 const controllers = require('./controllers/');
 
 router.get('/users', controllers.users.index);
+
 router.get('/events', controllers.events.index);
 router.get('/events/:id', controllers.events.get);
 router.post('/events/', controllers.events.create);
 router.delete('/events/:id', controllers.events.delete);
+router.post('/events/:id/present', controllers.events.present);
+router.post('/events/:id/absent', controllers.events.absent);
 
 router.post('/login', controllers.auth.login);
 
