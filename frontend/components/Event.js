bd99f69d4142eee96f3c8a16f9cad3f6832f77aa
Joe Torraca <joe@joetorraca.com>
2017-11-22 11:45:54 -0500

    Adds wrapperfor padding and description pruning

--- frontend/components/Event.js
+++ frontend/components/Event.js
@@ -9,6 +9,8 @@ import { deleteEvent } from '../actions';
 
 import { uniqueId } from 'lodash';
 
+import moment from 'moment';
+
 import Clearfix from './Clearfix';
 import {Edit} from './Buttons';
 
@@ -18,43 +20,52 @@ class Event extends Component {
   }
 
   render() {
-    const { data, deleteEvent, history } = this.props; 
+    const { data, deleteEvent, history } = this.props;
     const link = `${data.showAdminControls ? '/admin' : ''}/events/${data._id}`;
     return (
-    	<div key={uniqueId('event_')} onClick={() => history.push(link)}>
-	        <Segment key={uniqueId('event_')}>
-	            <h3>{data.name}</h3>
-	            <p>{data.description}</p>
-	            <p><Icon name='calendar'/>{data.datetime}</p>
-	            <p><Icon name='road'/>{data.location}</p>
-	            {data.showAdminControls &&
-	            <Clearfix>
-	                <Button.Group floated='right'>
-	                    <Edit history={history} route={`admin/events/${data._id}/edit`}/>
-	                    <Modal
-	                      trigger={
-	                        <Button animated="vertical" color="red">
-	                          <Button.Content visible>Delete</Button.Content>
-	                          <Button.Content hidden>
-	                            <Icon name='trash' />
-	                          </Button.Content>
-	                        </Button>
-	                      }
-	                      header='Confirm Delete'
-	                      content='Are you sure you want to delete this event?'
-	                      actions={[
-	                        'Cancel',
-	                        { key: 'done', content: 'Delete', negative: true },
-	                      ]}
-	                      onActionClick={() => deleteEvent(data._id)}
-	                    />
-	                </Button.Group>
-	            </Clearfix>
-	            }
-	        </Segment>
+        <div style={{paddingTop: 10, paddingBottom: 10}}>
+            <div key={uniqueId('event_')} onClick={() => history.push(link)}>
+                <Segment key={uniqueId('event_')}>
+                    <h3>{data.name}</h3>
+                    <p>{pruneDescription(data.description)}</p>
+                    <p><Icon name='calendar'/>{moment(new Date(data.datetime)).format('MMMM Do YYYY, h:mm a')}</p>
+                    <p><Icon name='road'/>{data.location}</p>
+                    {data.showAdminControls &&
+                        <Clearfix>
+                            <Button.Group floated='right'>
+                                <Edit history={history} route={`admin/events/${data._id}/edit`}/>
+                                <Modal
+                                    trigger={
+                                        <Button animated="vertical" color="red">
+                                            <Button.Content visible>Delete</Button.Content>
+                                            <Button.Content hidden>
+                                                <Icon name='trash' />
+                                            </Button.Content>
+                                        </Button>
+                                    }
+                                    header='Confirm Delete'
+                                    content='Are you sure you want to delete this event?'
+                                    actions={[
+                                        'Cancel',
+                                        { key: 'done', content: 'Delete', negative: true },
+                                    ]}
+                                    onActionClick={() => deleteEvent(data._id)}
+                                />
+                            </Button.Group>
+                        </Clearfix>
+                    }
+                </Segment>
+            </div>
 	    </div>
     )
   }
+}
+
+const pruneDescription = (description) => {
+    const cutoff = 20; // 20 words;
+    const split = description.split(' ');
+    if (split.length > cutoff) return `${split.splice(0, 20).join(' ')}...`;
+    return description;
 }
 
 const mapStateToProps = (state) => {
