689f3deab7ef21fbf2e5b8bfdfd55fcfcbf15224
Sam <sam.scheele@outlook.com>
2017-11-25 13:22:33 -0500

    fix namespace in routes

--- backend/routes.js
+++ backend/routes.js
@@ -5,14 +5,14 @@ const router = express.Router();
 const controllers = require('./controllers/');
 const auth = require('./auth');
 
-router.get('/users', auth.isAdmin, controllers.users.index);
+router.get('/users', auth.checkAdmin, controllers.users.index);
 
 router.get('/events', controllers.events.index);
 router.get('/events/:id', controllers.events.get);
-router.post('/events/', auth.isAdmin, controllers.events.create);
-router.delete('/events/:id', auth.isAdmin, controllers.events.delete);
-router.post('/events/:id/present', auth.isAdmin, controllers.events.present);
-router.post('/events/:id/absent', auth.isAdmin, controllers.events.absent);
+router.post('/events/', auth.checkAdmin, controllers.events.create);
+router.delete('/events/:id', auth.checkAdmin, controllers.events.delete);
+router.post('/events/:id/present', auth.checkAdmin, controllers.events.present);
+router.post('/events/:id/absent', auth.checkAdmin, controllers.events.absent);
 
 router.post('/login', controllers.auth.login);
 
