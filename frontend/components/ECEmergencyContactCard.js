89198b0d62b69c07dede20231a0d2fbfdecbd4e1
Joe Torraca <joe@joetorraca.com>
2017-11-26 18:58:17 -0500

    Add Emergency Contact card data

--- frontend/components/ECEmergencyContactCard.js
+++ frontend/components/ECEmergencyContactCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Segment, List } from 'semantic-ui-react';
 
 const ECEmergencyContactCard = ( props ) => {
 	const {user} = props;
@@ -12,14 +12,32 @@ const ECEmergencyContactCard = ( props ) => {
 			</Segment>
 		)
 	} else {
+		const emergencyFields = ['Name', 'Phone', 'Relation'];
 	    return (
-			<Segment></Segment>
+			<Segment>
+				<h3>Emergency Contact</h3>
+				<List >
+					{emergencyFields.map(d => {
+						return (
+							<List.Item key={d}>
+								<List.Content>
+									<b>{d}: </b>{nullCheck(user[`emergencyContact${d}`])}
+								</List.Content>
+							</List.Item>
+						)
+					})}
+				</List>
+			</Segment>
 	    );
 	}
 }
 
 const hasECData = (user) => {
 	return user.emergencyContactName || user.emergencyContactPhone || user.emergencyContactRelation;
+}
+
+const nullCheck = (data) => {
+	return data ? data : (<i>&lt;Unknown&gt;</i>);
 }
 
 export default ECEmergencyContactCard;
