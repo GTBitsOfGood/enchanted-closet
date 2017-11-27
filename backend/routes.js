150b23ce3655948668a3b1e0c6b28a955e48cd10
Sam <sam.scheele@outlook.com>
2017-11-27 16:45:19 -0500

    Change reporting route

--- backend/routes.js
+++ backend/routes.js
@@ -12,6 +12,6 @@ router.get('/events/:id', controllers.events.get);
 router.delete('/events/:id', controllers.events.delete);
 router.post('/attendance/present/:eventId/:userId', controllers.events.markPresentAt);
 
-router.get('/report/:id', auth.checkAdmin, reporting.generateReport);
+router.get('/events/:id/report', auth.checkAdmin, reporting.generateReport);
 
 module.exports = router;
