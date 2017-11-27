361a72dd6fc8e14c2875bc550ae2d04cf0fcd697
Joel Ye <joelye9@gmail.com>
2017-11-26 23:32:59 -0500

    tolerable front skin

--- frontend/containers/AppContainer.js
+++ frontend/containers/AppContainer.js
@@ -5,24 +5,36 @@ import Title from '../components/Title';
 import { FileForm } from '../components/CustomForm';
 import EventEntry from '../components/EventEntry';
 import Frame from '../components/Frame';
+import Navigation from './Navigation';
 
-import { Container, Grid, Reveal } from 'semantic-ui-react';
+import { Segment, Container, Grid, Reveal, Menu, Header, Button, Icon, Image } from 'semantic-ui-react';
 
 const AppContainer = () => {
-    return (
-    <div>
-        <Grid.Row>
-            <Grid.Column>
-                <Container>
-                    <Title/>
-                    <FileForm type="survey"/>
-                    <EventEntry title="a" date="b" desc="c" address="d" />
-                </Container>
-            </Grid.Column>
-        </Grid.Row>
-    </div>
-    );
+    const logoSrc = "/images/EC_logo_web.png"
+    
+    return (<Segment
+	textAlign='center'
+	style={{ minHeight: 700, padding: '1em 0em' }}
+	vertical
+	    >
+        <Container>
+	    <Image src={logoSrc} size='large' centered />
+            <Header
+                as='h2'
+                content='Volunteer Registration Platform'
+                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+            />
+	    <Header
+		as='h2'
+		content='Please Log In'
+		style={{ fontSize : '1.7em', fontWeight: 'normal' }}
+	    />
+        </Container>
+    </Segment>)
 };
+
+
+
 
 AppContainer.propTypes = {
 };
