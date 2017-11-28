42d82a5c5a63c00142d5466b98db532a4e388cfc
Joe Torraca <joe@joetorraca.com>
2017-11-28 02:39:25 -0500

    Ensure the localStorage store gets written to

--- frontend/index.js
+++ frontend/index.js
@@ -3,11 +3,18 @@ import { render } from 'react-dom';
 import { configureStore, history } from './store/configureStore';
 import Root from './containers/Root';
 
+import { loadState, saveState } from './store/localStorage';
+
 import '../node_modules/semantic-ui-css/semantic.min.css';
 
 import './assets/stylesheets/base';
 
-const store = configureStore();
+const store = configureStore(loadState());
+
+console.log(store);
+store.subscribe(() => {
+    saveState(store.getState());
+})
 
 render(
     <Root store={store} history={history} />,
