26d37f1274a62d912a3ef51a503679df86505752
jaltaffer99 <jordan.altaffer@gmail.com>
2018-02-13 12:49:01 -0600

    added search function

--- frontend/containers/Events.js
+++ frontend/containers/Events.js
@@ -7,7 +7,7 @@ import Radium from 'radium';
 
 import { fetchEventsIfNeeded, invalidateEvents, fetchEvents } from '../actions/index';
 
-import { Button, Container, Icon, Dimmer, Loader, Segment, Header } from 'semantic-ui-react';
+import { Button, Container, Icon, Dimmer, Loader, Segment, Header, Input, Search} from 'semantic-ui-react';
 import { Event } from '../components/';
 
 
@@ -15,6 +15,7 @@ import { Event } from '../components/';
 class Events extends Component {
   constructor(props) {
     super(props);
+    this.state = {query: '', filters: {'Name': false, 'Location': false}}
     this.handleRefreshClick = this.handleRefreshClick.bind(this)
   }
 
@@ -23,13 +24,21 @@ class Events extends Component {
     dispatch(fetchEvents());
     // dispatch(fetchEventsIfNeeded());
   }
-
+  search(event) {
+    this.setState({query: event.target.value})
+  }
   handleRefreshClick(e) {
     e.preventDefault()
 
     const { dispatch } = this.props;
     dispatch(invalidateEvents())
     dispatch(fetchEventsIfNeeded());
+  }
+  changeQuery(data)
+  {
+    var filts = this.state.filters;
+    filts[data.label] = !filts[data.label];
+    this.setState({filters: filts})
   }
 
   render() {
@@ -43,11 +52,49 @@ class Events extends Component {
       <Container>
 	<Segment textAlign="center" style={styles.base}>
 	  <Header as="h1">Upcoming Events</Header>
-	</Segment>
-	<Loader active={isFetchingEvents}>Loading</Loader>
+	</Segment> 
+  
+  <Input
+    placeholder = 'Search'
+    type = 'text'
+    size = 'big'
+    onChange={
+        (e) => this.search(e)
+    }/> 
+  <Segment textAlign='left' vertical >
+  <Header as="l1">Search By:</Header>
+  </Segment>
+  <Segment textAlign='left' vertical >
+  <Button
+    active = {this.state.filters['Name']}
+    label = 'Name'
+    labelPosition = 'left'
+    size = 'small'
+    toggle
+    onClick={
+      (e, data) => (this.changeQuery(data))
+    }/>
+  <Button
+    active = {this.state.filters['Location']}
+    label = 'Location'
+    labelPosition = 'left'
+    size = 'small'
+    toggle
+    onClick={
+      (e, data) => (this.changeQuery(data))
+    }/>
+  </Segment>
+  <Loader active={isFetchingEvents}>Loading</Loader>
 	{ events.length > 0 &&
-	  events.map(e => {
-	    return <Event key={e._id} data={e} history={history}/>
+	    events.map(e => {
+      if (this.state.filters['Name'] == true)
+        if (e.name.toLowerCase().includes(this.state.query.toLowerCase()))
+          return <Event key={e._id} data={e} history={history}/>
+      if (this.state.filters['Location'] == true)
+        if (e.location.toLowerCase().includes(this.state.query.toLowerCase()))
+          return <Event key={e._id} data={e} history={history}/>
+      if (this.state.filters['Name'] == false && this.state.filters['Location'] == false)
+        return <Event key={e._id} data={e} history={history}/>
 	  })
 	}
 	{ !isFetchingEvents && events.length === 0 &&
