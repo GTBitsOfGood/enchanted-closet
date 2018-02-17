6f4af62395eb57046d7dcfe74490ccb0f0606872
jaltaffer99 <jordan.altaffer@gmail.com>
2018-02-17 16:34:32 -0600

    idek

--- frontend/containers/Events.js
+++ frontend/containers/Events.js
@@ -1,5 +1,7 @@
+    dispatch(fetchEvents());
+    dispatch(fetchEvents());
 import PropTypes from 'prop-types';
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {uniqueId} from 'lodash';
 import { withRouter } from 'react-router-dom';
@@ -7,15 +9,12 @@ import Radium from 'radium';
 
 import { fetchEventsIfNeeded, invalidateEvents, fetchEvents } from '../actions/index';
 
-import { Button, Container, Icon, Dimmer, Loader, Segment, Header, Input, Search} from 'semantic-ui-react';
-import { Event } from '../components/';
-
-
+import { Button, Container, Icon, Dimmer, Segment, Header } from 'semantic-ui-react';
+import { EventBody } from '../components/';
 
 class Events extends Component {
   constructor(props) {
     super(props);
-    this.state = {query: '', filters: {'Name': false, 'Location': false}}
     this.handleRefreshClick = this.handleRefreshClick.bind(this)
   }
 
@@ -24,21 +23,13 @@ class Events extends Component {
     dispatch(fetchEvents());
     // dispatch(fetchEventsIfNeeded());
   }
-  search(event) {
-    this.setState({query: event.target.value})
-  }
+
   handleRefreshClick(e) {
     e.preventDefault()
 
     const { dispatch } = this.props;
     dispatch(invalidateEvents())
     dispatch(fetchEventsIfNeeded());
-  }
-  changeQuery(data)
-  {
-    var filts = this.state.filters;
-    filts[data.label] = !filts[data.label];
-    this.setState({filters: filts})
   }
 
   render() {
@@ -48,60 +39,8 @@ class Events extends Component {
       e.showAdminControls = false;
       return e;
     });
-    return (
-      <Container>
-	<Segment textAlign="center" style={styles.base}>
-	  <Header as="h1">Upcoming Events</Header>
-	</Segment> 
-  
-  <Input
-    placeholder = 'Search'
-    type = 'text'
-    size = 'big'
-    onChange={
-        (e) => this.search(e)
-    }/> 
-  <Segment textAlign='left' vertical >
-  <Header as="l1">Search By:</Header>
-  </Segment>
-  <Segment textAlign='left' vertical >
-  <Button
-    active = {this.state.filters['Name']}
-    label = 'Name'
-    labelPosition = 'left'
-    size = 'small'
-    toggle
-    onClick={
-      (e, data) => (this.changeQuery(data))
-    }/>
-  <Button
-    active = {this.state.filters['Location']}
-    label = 'Location'
-    labelPosition = 'left'
-    size = 'small'
-    toggle
-    onClick={
-      (e, data) => (this.changeQuery(data))
-    }/>
-  </Segment>
-  <Loader active={isFetchingEvents}>Loading</Loader>
-	{ events.length > 0 &&
-	    events.map(e => {
-      if (this.state.filters['Name'] == true)
-        if (e.name.toLowerCase().includes(this.state.query.toLowerCase()))
-          return <Event key={e._id} data={e} history={history}/>
-      if (this.state.filters['Location'] == true)
-        if (e.location.toLowerCase().includes(this.state.query.toLowerCase()))
-          return <Event key={e._id} data={e} history={history}/>
-      if (this.state.filters['Name'] == false && this.state.filters['Location'] == false)
-        return <Event key={e._id} data={e} history={history}/>
-	  })
-	}
-	{ !isFetchingEvents && events.length === 0 &&
-	  <h1>No events</h1>
-	}
-      </Container>
-    );
+    const bodyProps = { events, isFetchingEvents };
+    return (<EventBody { ...bodyProps } />);
   }
 }
 
@@ -110,12 +49,6 @@ Events.propTypes = {
   isFetchingEvents: PropTypes.bool.isRequired,
   lastUpdatedEvents: PropTypes.number,
   dispatch: PropTypes.func.isRequired
-}
-
-const styles = {
-  base: {
-    padding: "2em"
-  }
 }
 
 function mapStateToProps(state) {
