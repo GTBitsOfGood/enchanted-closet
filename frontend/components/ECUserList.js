d1d5c634da642190e3522a77f1d64c605717b67f
jaltaffer99 <jordan.altaffer@gmail.com>
2018-04-12 14:42:04 -0500

    done with attendance

--- frontend/components/ECUserList.js
+++ frontend/components/ECUserList.js
@@ -1,33 +1,42 @@
-import React,{Component} from 'react';
-import {connect} from 'react-redux';
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
 
 import { LoadingIcon } from './';
 
-import {Segment, List, Checkbox} from 'semantic-ui-react';
+import { Segment, List } from 'semantic-ui-react';
 
 import ECUserListItem from './ECUserListItem';
 
 const ECUserList = ( props ) => {
   const {users, event, filter} = props;
+
   return (
     <Segment>
-      {users &&
+      {users ?
        <List selection verticalAlign='middle'>
 	 {users.map(user => {
-	 	const attending = event.participantsAttended.filter(u => u === user._id).length === 1;
-	    return (<ECUserListItem
-		      attending={attending}
-		      event={event}
-		      key={user._id}
-		      user={user}
-		      filter={filter}
-	    />);
+	    const isAttending = user.role === 'Volunteer' ?
+				event.volunteersAttended.filter(v => v === user._id).length === 1 :
+				event.participantsAttended.filter(p => p === user._id).length === 1;
+	    return (
+	      <ECUserListItem
+		initAttending={isAttending}
+		event={event}
+		key={user._id}
+		user={user}
+		filter={filter}
+	      />);
 	 })}
        </List>
-      }
-      {!users && <LoadingIcon active />}
+       : <LoadingIcon active />}
     </Segment>
   );
 }
+
+const styles = {
+  "listWrap": {
+  }
+}
+
 
 export default ECUserList;
--- frontend/containers/Admin/Attendance.js
+++ frontend/containers/Admin/Attendance.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 // TODO: Disallow editing of past/future events
 import { fetchFutureEvents, fetchPastEvents, fetchUsers } from '../../actions/';
 
-import { Container, Segment } from 'semantic-ui-react';
+import { Container, Segment, Header } from 'semantic-ui-react';
 import { ErrorComponent, LoadingIcon, PageTitle, SearchBarCard, UserList } from '../../components' 
 
 class AdminAttendance extends Component {
@@ -20,12 +20,6 @@ class AdminAttendance extends Component {
       loading: false,
       filter: ''
     };
-    if (events && events.length > 0) {
-      const event = events.filter(e => e._id === this.state.event_id);
-      if (event && event.length === 1) {
-	this.state.event = event[0];
-      }
-    }
 
     this.searchFilterUsers = this.searchFilterUsers.bind(this);
   }
@@ -80,8 +74,41 @@ class AdminAttendance extends Component {
   }
 
   render() {
-    const { loading, event, users, hasAttemptedRefresh, filter } = this.state;
-    if (event) {
+    const { event, loading, hasAttemptedRefresh, filter } = this.state; // event in state is pretty suboptimal
+    const { users, user } = this.props;
+    if (user && user.role === 'Volunteer') { // Check legality
+      const isRegistered = event && event.volunteers.includes(user._id);
+      if (!isRegistered) {
+	// redirect
+	return <Redirect to="/events" />;
+      }
+    }
+    
+    const volunteerBlock = user && users && user.role === 'Admin' && (
+      <div>
+	<Header as="h4">Volunteers </Header>
+	<UserList event={event} users={users.filter(user => user.role === "Volunteer")} filter={filter} />
+      </div>
+    );
+    const participantBlock = users && (
+      <div>
+	<Header as="h4">Participants </Header>
+	<UserList event={event} users={users.filter(user => user.role === "Participant")} filter={filter} />
+      </div>
+    );
+    // TODO: locking and redirect
+
+    if (event && user) {
+      
+      const isAttending = event.volunteersAttended.filter(v => v._id === user._id).length === 1;
+      if (user.role === 'Volunteer' && !isAttending) {
+	// lock
+	return (<Container>
+	  <Segment>
+	    Looks like you're registered! Have a coordinator sign you in to start marking participant attendance!
+	  </Segment>
+	</Container>);
+      }
       return (
 	<Container>
 	  <PageTitle
@@ -91,7 +118,8 @@ class AdminAttendance extends Component {
 	    linkTitle="Register"
 	  />
 	  <SearchBarCard filterFunction={this.searchFilterUsers}/>
-	  <UserList event={event} users={users} filter={filter} />
+	  {volunteerBlock}
+	  {participantBlock}
 	</Container>
       );
     } else {
